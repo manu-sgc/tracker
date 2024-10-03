676d9b2375c2462ca4f6929940f40573b7995c54
manu-sgc <manu.silvestregc@gmail.com>
2024-10-03 13:34:08 -0300

    adding the pass the page logic

--- atividades/urls.py
+++ atividades/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.tracker_view, name='tracker'),
+    path('calendario/', views.calendario_view, name='calendario'),
+    path('avaliacao/', views.avaliacao_view, name='avaliacao'),
+    path('leitura/', views.leitura_view, name='leitura'),
+    path('adulting/', views.atividades_view, name='adulting'),
+    path('habitos/', views.habitos_view, name='habitos'),
+    path('sono/', views.sono_view, name='sono'),
+    path('destaque/', views.destaque_view, name='destaque'),
+    path('filmes_series/', views.filmes_series_view, name='filmes_series'),
 ]
--- atividades/views.py
+++ atividades/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def tracker_view(request):
     return render(request, 'atividades/tracker.html')
+
+def avaliacao_view(request):
+    return render(request, 'atividades/avaliacao.html')
+
+def leitura_view(request):
+    return render(request, 'atividades/leitura.html')
+
+def atividades_view(request):
+    return render(request, 'atividades/adulting.html')
+
+def habitos_view(request):
+    return render(request, 'atividades/habitos.html')
+
+def sono_view(request):
+    return render(request, 'atividades/sono.html')
+
+def destaque_view(request):
+    return render(request, 'atividades/destaque.html')
+
+def filmes_series_view(request):
+    return render(request, 'atividades/filmes_series.html')
+
+def calendario_view(request):
+    return render(request, 'atividades/calendario.html')
